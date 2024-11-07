d0037c4f508d93191ab3ea6216b7ec49bf36ed16
stefa <stefa@DESKTOP-DBAR1K9>
2024-11-07 12:13:47 +0100

    Modificacion del nombre de las clases

--- src/mi_modelo/Pelu_Modelo.java
+++ src/mi_modelo/Pelu_Modelo.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package controlador;
+package mi_modelo;
 
 /**
  *
  * @author dam2
  */
-public class ClaseControlador {
+public class Pelu_Modelo {
     
 }
